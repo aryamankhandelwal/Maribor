7b9b48339ddefec43091f276075cdd39de5e0e89
Aryaman <aryaman99@gmail.com>
2025-07-30 19:47:21 +0400

    Initial Commit

---
+++ Maribor/MariborApp.swift
@@ -0,0 +1,17 @@
+//
+//  MariborApp.swift
+//  Maribor
+//
+//  Created by Aryaman on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MariborApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MariborTests/MariborTests.swift
@@ -0,0 +1,17 @@
+//
+//  MariborTests.swift
+//  MariborTests
+//
+//  Created by Aryaman on 7/30/25.
+//
+
+import Testing
+@testable import Maribor
+
+struct MariborTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
